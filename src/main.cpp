1b5ed691c5383f9df4fb36711048c4586432bf58
Phario <grzedyn@gmail.com>
2025-04-20 01:08:45 +0200

    major changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,13 @@
 #include <Arduino.h>
-
+/*
+Hex values for remote buttons
+Left arrow:BB44FF00
+Right arrow:BC43FF00
+Up arrow:B946FF00
+Down arrow:EA15FF00
+OK button:BF40FF00
+Library link: https://github.com/Arduino-IRremote/Arduino-IRremote
+*/
 void setup() {
   // Setup here
   Serial.begin(115200);
@@ -17,7 +25,7 @@ void loop() {
       {0, 1, 1, -1, -1, 1, 1, 0},
       {0, 1, 1, -1, -1, 1, 1, 0}
     };
-
+  
   int rbSize = 8;
 
   free(data);
