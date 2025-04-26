e28f9977d61c2c3865913e8aa7b60c6fae6f8f68
Filip <filipkwiek888@gmail.com>
2025-04-26 20:54:18 +0200

    Add sample code to test leds

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
 #include <Arduino.h>
+#include <FastLED.h>
+#include "InputHandler.h"
 
+#define FAST_LED_PIN 11 
+
+FASTLED_USING_NAMESPACE
 /*
 Hex values for remote buttons
 Left arrow:BB44FF00
@@ -10,14 +15,27 @@ OK button:BF40FF00
 Library link: https://github.com/Arduino-IRremote/Arduino-IRremote
 */
 
-void setup() {
+CRGB leds[64];
+
+void setup() 
+{
   // Setup here
   Serial.begin(115200);
   // Serial.println("Hello World!");
   // create 8x8 array
-  
+
+  int** rbArr = create2DArray(8, 8);
+
+
+
+  delay(3000);
+  FastLED.addLeds<WS2812B, FAST_LED_PIN, GRB>(leds, 64);
 }
 
-void loop() {
+uint8_t hue = 0;
 
+void loop() 
+{
+  fill_rainbow(leds, 64, hue, 7);
+  FastLED.show();
 }
